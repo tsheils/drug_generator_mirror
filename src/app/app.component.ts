e45f7d9cd28fc8f6eedae39bc944cb0881f6cf9d
Tim Sheils <timothy.sheils@nih.gov>
2018-02-02 16:52:13 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {SafeResourceUrl, DomSanitizer} from "@angular/platform-browser";
+import {HttpClient} from "@angular/common/http";
+import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+/*
+  @ViewChild('sketcher') sketchTarget: ElementRef;
+*/
+  marvinSketcherInstance;
+
   title = 'app';
+  data: {};
+
+  url: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer,
+              private http: HttpClient) {
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl('./assets/vendor/marvin/editorws.html');
+  }
+  ngOnInit(){
+    let ctrl = this;
+    window['MarvinJSUtil'].getEditor('#sketcher').then(function (marvin) {
+      console.log(marvin);
+      ctrl.marvinSketcherInstance = marvin;
+      ctrl.marvinSketcherInstance.on("molchange", function(){
+
+      })
+    }, function (err) {
+      console.log(err);
+    });
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+
+  submit() {
+    let ctrl = this;
+    ctrl.marvinSketcherInstance.exportStructure("mol").then(function(source){
+      console.log(source);
+      ctrl.http.post('https://predictor.ncats.io/route/predx/structurePredict', source).subscribe(res => {
+        console.log(res[0]);
+        ctrl.data = res[0];
+      })
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MaterialModule} from "../assets/material/material.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
