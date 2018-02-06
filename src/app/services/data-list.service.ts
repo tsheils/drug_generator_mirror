a9fd5b4e4247eb2d50027e4602a7139e8124efc7
Tim Sheils <timothy.sheils@nih.gov>
2018-02-06 17:33:19 -0500

    add models list

--- src/app/services/data-list.service.ts
+++ src/app/services/data-list.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Subject} from "rxjs/Subject";
-import {Data} from "../models/data";
+import {Subject} from 'rxjs/Subject';
+import {Data} from '../models/data';
 
 @Injectable()
 export class DataListService {
